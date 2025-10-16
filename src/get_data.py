5530e2d74ebc0c3072feeb9b7b2d85f303bc7857
cameronpaschal <cpaschy@byui.edu>
2025-10-16 14:50:37 -0600

    added features

--- src/get_data.py
+++ src/get_data.py
@@ -27,17 +27,60 @@ def CleanData(df, stockName):
     clean_frame = df[["Ticker", "Close", "ret_1m"]]
 
     return clean_frame
+
+def AddFeatures(df):
+    df["ret_1m_lag1"] = df["ret_1m"].shift(1)
+    df["ret_3m_lag1"] = (
+        df["ret_1m"]
+        .rolling(window=3, min_periods=3)
+        .sum()
+        .shift(1)
+    )
+    df["ret_12m_lag1"] = (
+        df["ret_1m"]
+        .rolling(window=12, min_periods=12)
+        .sum()
+        .shift(1)
+    )
+    df["vol_3m_lag1"] = df["ret_1m"].rolling(3).std().shift(1)
+    df["month_of_year"] = df.index.month
+    
+    return df
+
     
 dia_clean = CleanData(dia, "DIA")
 qqq_clean = CleanData(qqq, "QQQ")
 spy_clean = CleanData(spy, "SPY")
 
+dia_clean["ret_1m_lag1"] = dia_clean["ret_1m"].shift(1)
+dia_clean["ret_3m_lag1"] = (
+    dia_clean["ret_1m"]
+    .rolling(window=3, min_periods=3)
+    .sum()
+    .shift(1)
+    )
+dia_clean["ret_12m_lag1"] = (
+        dia_clean["ret_1m"]
+        .rolling(window=12, min_periods=12)
+        .sum()
+        .shift(1)
+    )
+dia_clean["vol_3m_lag1"] = dia_clean["ret_1m"].rolling(3).std().shift(1)
+dia_clean["month_of_year"] = dia_clean.index.month
 
-comb = pd.concat([dia_clean, qqq_clean, spy_clean]).sort_index()
+
+
+
+# comb = pd.concat([dia_clean, qqq_clean, spy_clean]).sort_index()
+
+# comb["ret_1m_lag1"] = comb["ret_1m"].shift(1)
+# comb["ret_3m_lag1"] = comb["ret_1m"].apply(lambda x: x.tail(3).sum())
 
 
 
     
-print(comb.head)
+print(dia_clean)
+
+
 
 
